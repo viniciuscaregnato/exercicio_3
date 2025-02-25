9216ad52c23aa3c68f50bb166ffa7f61d6b2ef8d
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-02-25 20:36:09 -0300

    Add files via upload

---
+++ exercicio_3.py
@@ -0,0 +1,20 @@
+"""exercicio 3
+autor: vinicius
+versão: 0.0.1"""
+# Alocação de memoria
+
+numero_1 = ""
+numero_2 = ""
+
+# Entrada de dados
+
+numero_1 = int(input("digite um numero: "))
+numero_2 = int(input("digite outro numero: "))
+
+# Processamento de dados
+
+resultado = numero_1 + numero_2
+
+
+#Saída de dados
+print(resultado)
